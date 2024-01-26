849f89c9919b073e9123b3767640c9475d4dcd94
nerdybird2002 <157816591+nerdybird2002@users.noreply.github.com>
2024-01-26 12:53:31 -0500

    Update sketch.js
    attempt at the art stuff

--- sketch.js
+++ sketch.js
@@ -4,7 +4,28 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background('#fea');
   circle(200,200,50);
-  circle(400,200,50);
+  stroke('#2785E2')
+  ellipse(50,75,80,185);
+  stroke(255, 204, 0);
+strokeWeight(4);
+rect(20, 40, 60, 60);
+  quad(90, 90, 55, 90, 20, 15, 10, 5);
+  strokeWeight(10);
+line(20, 70, 80, 70);
+  triangle(400, 300, 400, 20, 300, 75);
+  rectMode(3)
+  line(200,200,50);
+  stroke('#EB25DD')
+  ellipse(100,30,30,30);
+  quad(200, 350, 86, 50, 50, 38, 14, 50);
+  fill('#E090DE')
+  strokeWeight(10);
+line(20, 70, 80, 70);
+  circle(300,300,300)
+    noFill()
+ circle(50,60,70)
+  triangle(200,350,300)
+  
 }
